0d66322abde1a6d11b8c684ea0cbe3028fbb15bb
Federico Elicer <FElicer@student.fullsail.edu>
2022-05-23 14:29:54 -0500

    added __vm and Release to gitignore

--- GeoCacheProject/Release/Adafruit_NeoPixel-master/esp.c.d
+++
@@ -1,2 +0,0 @@
-C:\Users\foodl\AppData\Local\Temp\VMBuilds\GeoCacheProject\uno\Release\Adafruit_NeoPixel-master\esp.c.o: \
- D:\School\Full\ Sail\Microcontrollers\Workspace\libraries\Adafruit_NeoPixel-master\esp.c
